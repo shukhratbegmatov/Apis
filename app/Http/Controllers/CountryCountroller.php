d1960d5c9326939dc2b002583c714db78b64fcf5
shukhrat <shukhratbegmatov2147@gmail.com>
2021-06-09 12:44:35 +0500

    post api requered

--- app/Http/Controllers/CountryCountroller.php
+++ app/Http/Controllers/CountryCountroller.php
@@ -10,9 +10,14 @@ class CountryCountroller extends Controller
 {
     function country($id = null)
     {
+    	
         return $id ? Country::find($id) : Country::all();
     }
     function store(Request $request){
+    	$request->validate([
+    		'id'=>'required',
+    		'name'=>'required'
+    	]);
     	return Country::create($request->all());	
     }
 }
